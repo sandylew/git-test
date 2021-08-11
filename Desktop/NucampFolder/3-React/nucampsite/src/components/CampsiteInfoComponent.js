9d58fb42c6f614af8f8e662ace6ef321839a8a80
Sandy Lewandowski <sandy_lew@yahoo.com>
2021-08-11 17:44:47 -0500

    Fetch From Server

--- Desktop/NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Button, Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, 
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -12,7 +13,7 @@ function RenderCampsite({ campsite }) {
     return (
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg src={campsite.image} alt={campsite.name} />
+                <CardImg src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
