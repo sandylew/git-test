d87a1dd9b3561c752fb1af973344e8227423e519
Sandy Lewandowski <sandy_lew@yahoo.com>
2021-08-11 20:00:22 -0500

    Fetch Post Comment

--- Desktop/NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -22,7 +22,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <span className="col-md-5 m-1">
@@ -36,7 +36,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                 </div>
                 )
                 }
-                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+                <CommentForm campsiteId={campsiteId} postComment={postComment}/>
             </span>
         );
     }
@@ -81,7 +81,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id} 
                     />
                 </div>
@@ -114,7 +114,7 @@ class CommentForm extends React.Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
 
     }
 
